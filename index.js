5352c0b30bbb890418ed984d0e097bb0c47c3445
Kid48 <21201234a@gmail.com>
2023-07-16 23:19:37 +0200

    Изменил реализацию и сделал ее на blessed

--- index.js
+++ index.js
@@ -1,130 +1,152 @@
 #!/usr/bin/env node
 
-import inquirer from "inquirer";
-import {createSpinner} from "nanospinner";
-import figlet from 'figlet';
 import axios from "axios";
-import boxen from "boxen";
+import blessed from "blessed";
 import {convert} from 'html-to-text';
-import chalk from "chalk";
 import he from 'he';
-import terminalImage from 'terminal-image';
-import got from 'got';
 
-
-const subreddit = process.argv[2] ? process.argv[2] : 'Privacy' ;
-
-const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms))
+const subreddit = process.argv[2] ? process.argv[2] : 'Privacy';
 
 const removeNonStandardTags = (text) => {
-    text =  text.replace(/<!--[\s\S]*?-->/g, '')
+    text = text.replace(/<!--[\s\S]*?-->/g, '')
         .replace(/<div class="md">/g, '')
         .replace(/<\/div>/g, '')
-        .replace(/<p>/g, '\n')
+        .replace(/<p>/g, '')
         .replace(/<\/p>/g, '\n')
         .replace(/<a[^>]*>(.*?)<\/a>/g, '$1');
 
     return he.decode(text)
 }
 
+// Create a screen object.
+var screen = blessed.screen({
+    smartCSR: true
+});
 
-async function welcome() {
-    console.log(
-        figlet.textSync("Teddit CLI", {
-            font: "Larry 3D 2",
-            horizontalLayout: "default",
-            verticalLayout: "default",
-            width: 100,
-            whitespaceBreak: true,
+screen.title = 'Teddit CLI';
 
-        })
-    );
-
-    await sleep();
-
-}
-
-async function get_post() {
-    const spinner = createSpinner('Loading posts... ').start();
-    const response = await axios.get(`https://teddit.net/r/${subreddit}?api`);
-    spinner.success({text: 'Posts loaded'});
-    const postsObject = response.data.links;
-
-
-    const postsArray = Object.entries(postsObject).map(([key, value]) => {
-        return {key, value};
-    });
-    //console.log(postsArray);
-    const postChoices = postsArray.map((post, index) => {
-        return {
-            name: post.value.title, value: index
-        };
-    });
-
-
-    const {selectedPost} = await inquirer.prompt([
-        {
-            type: 'list',
-            name: 'selectedPost',
-            message: 'Choose a post:',
-            choices: postChoices
+// Create a box to display the list of posts.
+var list = blessed.list({
+    top: 0,
+    left: 0,
+    width: '40%',
+    height: '100%',
+    items: ['Loading...'],
+    keys: true,
+    vi: true,
+    mouse: true,
+    style: {
+        selected: {
+            bg: 'blue'
         }
-    ]);
+    }
+});
 
-    const chosenPost = postsArray[selectedPost];
+// Create a box to display the selected post.
+var box = blessed.box({
+    top: 0,
+    left: '40%',
+    width: '60%',
+    height: '50%',
+    content: '',
+    tags: true,
+    border: {
+        type: 'line'
+    },
+    style: {
+        fg: 'white',
+        bg: 'black',
+        border: {
+            fg: '#f0f0f0'
+        }
+    }
+});
+var comments = blessed.box({
+    top: '50%',
+    left: '40%',
+    width: '60%',
+    height: '100%',
+    content: '',
+    tags: true,
+    border: {
+        type: 'line'
+    },
+    style: {
+        fg: 'white',
+        bg: 'black',
+        border: {
+            fg: '#f0f0f0'
+        }
+    }
+});
 
+// Append our boxes to the screen.
+screen.append(list);
+screen.append(box);
+screen.append(comments);
 
-    const title = chosenPost.value.title;
-    const text = chosenPost.value.selftext_html ? removeNonStandardTags(convert(chosenPost.value.selftext_html, {wordwrap: 130})) + "\n" : "";
-    const author = chosenPost.value.author;
-    const url = chosenPost.value.url && chosenPost.value.url.includes('teddit.net') ? chosenPost.value.url : "";
-    const image_link = chosenPost.value.images ? await got(chosenPost.value.images.thumb).buffer()  : '';
-    const image = image_link ? await terminalImage.buffer(image_link) : '';
+// Quit on Escape, q, or Control-C.
+screen.key(['escape', 'q', 'C-c'], function (ch, key) {
+    return process.exit(0);
+});
 
+// Fetch posts from Teddit.
+const teddit_instance = "https://teddit.pussthecat.org"
+axios.get(`${teddit_instance}/r/${subreddit}?api`)
+    .then(function (response) {
+        const postsObject = response.data.links;
+        const postsArray = Object.entries(postsObject).map(([key, value]) => {
+            return {key, value};
+        });
 
-    console.log(boxen(title, {title: chalk.blue('u/' + author)}));
-    console.log(
-        boxen(
-             image + "\n"+ text + url , {
-                padding: 1,
-                borderStyle: 'double'
-            }
-        )
-    );
+        // Update the list items with the post titles.
+        list.setItems(postsArray.map(post => post.value.title));
+        screen.render();
 
+        // Display the selected post when a list item is selected.
+        list.on('select', function (item, index) {
+            const chosenPost = postsArray[index];
+            const title = chosenPost.value.title;
+            const text = chosenPost.value.selftext_html ? removeNonStandardTags(convert(chosenPost.value.selftext_html, {wordwrap: 130})) + "\n" : "";
+            const author = chosenPost.value.author;
+            const url = chosenPost.value.url && !chosenPost.value.url.includes('teddit.net') ? chosenPost.value.url : "";
+            box.setContent(`Title: ${title}\nAuthor: ${author}\n\n${text}${url}`);
+            comments.content = '';
+            get_comments(chosenPost.value.id);
 
-    await get_comments(chosenPost.value.id);
-}
+            screen.render();
+        });
+    })
+    .catch(function (error) {
+        // Update the list items with the error message.
+        list.setItems(['Error: ' + error.message]);
+        screen.render();
+    });
 
 async function get_comments(post_id) {
-    const spinner = createSpinner('Loading comments... ').start();
     const response = await axios.get(`https://teddit.net/r/privacy/comments/${post_id}?api`);
-    spinner.success({text: 'Comments loaded'});
-
     const commentsObject = response.data.comments;
 
-    commentsObject ? display_comments(commentsObject) : console.log("Пока нету комментов");
-
+    if (commentsObject) {
+        display_comments(commentsObject);
+    } else {
+        comments.content = "No comments.";
+        screen.render();
+    }
 }
 
 function display_comments(comments, depth = 0) {
     const padding = ' '.repeat(depth * 2);
     for (const comment of comments) {
         const formattedComment = `${removeNonStandardTags(convert(comment.body_html, {wordwrap: 130}))}`;
-        const boxenOptions = {
-            // padding: depth,
-            margin: { left: depth*2},
-            borderStyle: 'round',
-            title: `u/${comment.author}`
-        };
-        const boxedComment = boxen(formattedComment, boxenOptions);
-        console.log(boxedComment);
+        comments.content += "\n\n" + padding + formattedComment;
         if (comment.replies && comment.replies.length > 0) {
             display_comments(comment.replies, depth + 1);
         }
     }
+    screen.render();
 }
 
-console.clear();
-await welcome();
-await get_post();
+// Focus our list.
+list.focus();
+
